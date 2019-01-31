5001c5202be25f1e37e0cd6066ffa071bab775a1
Ben Parisot <me@benparisot.com>
2019-01-31 11:31:06 -0800

    get color button node from dom and create colors array

---
+++ src/index.js
@@ -0,0 +1,19 @@
+//get color-button node
+const colorButtonsNode = document.getElementById('color-buttons');
+
+
+//create array of colors
+const colors = [
+    'blue',
+    'green',
+    'red',
+    'yellow'
+];
+
+for(let index = 0; index < colors.length; index++) {
+    let color = colors[index];
+    const colorButton = document.createElement('button');
+    console.log(colorButton);
+}
+
+//create button elements 
